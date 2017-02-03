9c7fecebe22aefc899ac46988a753d2ec80a817e
Radegost <konrad.chojnecki@gmail.com>
2017-02-03 01:09:39 +0100

    done

--- app/controllers/profileController.js
+++ app/controllers/profileController.js
@@ -1,4 +1,54 @@
 (function () {
     "use strict";
+    angular.module('app').controller('ModalCtrl', function ($uibModal, $log, $document) {
+        var $ctrl = this;
+
+        $ctrl.animationsEnabled = true;
+
+        $ctrl.open = function (id, parentSelector) {
+            $ctrl.id = id;
+            var parentElem = parentSelector ?
+                angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+            var modalInstance = $uibModal.open({
+                templateUrl: 'app/templates/profile.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$ctrl',
+                appendTo: parentElem,
+                resolve: {
+                    id : function () {
+                        return $ctrl.id;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $ctrl.selected = selectedItem;
+            }, function () {
+            });
+        };
+
+
+
+
+
+    });
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+    angular.module('app').controller('ModalInstanceCtrl', function ($uibModalInstance,$http, id) {
+        var $ctrl = this;
+        $http.get("data/modal"+id+".json").then(function (response) {
+            $ctrl.data = response.data;
+            console.log(response.data);
+        });
+        $ctrl.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    });
+
+// Please note that the close and dismiss bindings are from $uibModalInstance.
+
+
 
 })();
